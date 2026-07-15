18ba29da1e92577b6d8f55803f8936fb11aac0de
Vilhjálmur Hallgrímsson <vlhjalmur.hallgrimsson>
2026-07-15 19:02:25 +0000

    Update homepage hero and corporate design

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -22,5 +22,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/awards`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
